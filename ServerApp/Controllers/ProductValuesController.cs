be0f11865063655edc351e97cfd20dd8ffd3daf6
Katherine Hambley <superdesigngirl@mac.com>
2021-02-03 17:22:21 -0600

    Created the web service GetProduct endpoint

---
+++ ServerApp/Controllers/ProductValuesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ServerApp.Models;
+
+namespace ServerApp.Controllers
+{
+	
+	[Route("api/products")]
+	[ApiController]
+	public class ProductValuesController : Controller
+	{
+		private DataContext _context;
+		public ProductValuesController(DataContext context)
+		{
+			_context = context;
+		}
+
+		//web service endpoint https://localhost:5001/api/products/1
+		[HttpGet("{id}")]
+		public Product GetProduct(long id)
+		{
+			// This is to smoke test the web service endpoint
+			return _context.Products.Find(id);
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}

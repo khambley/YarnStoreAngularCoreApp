91cd460db36cb4860cecc4ea8b1dae1a84043aa0
Katherine Hambley <superdesigngirl@mac.com>
2021-02-04 08:42:50 -0600

    Added HTTP web service in Angular

--- ClientApp/src/app/models/model.module.ts
+++ ClientApp/src/app/models/model.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Repository } from "./repository";
+import { HttpClientModule } from '@angular/common/http';
 
 
 // This is the decorator. The NgModule decorator tells Angular that ModelModule is a feature module.
 @NgModule({
+  imports: [HttpClientModule],
   // Configuration property used to register classes for DI.
   providers: [Repository]
 })
--- ClientApp/src/app/models/repository.ts
+++ ClientApp/src/app/models/repository.ts
@@ -1,9 +1,23 @@
 import { Product } from "./product.model";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
+@Injectable() //Only used when there is a dependency injected into the constructor. pg. 80
 export class Repository {
-  constructor() {
-    // this is just to test to see if the repository is working, no HTTP web service yet. pg.60
-    this.product = JSON.parse(document.getElementById("data").textContent);
-  }
   product: Product;
+
+  // support for HTTP requestsis provided by the HTTPClient class, DI here.
+  constructor(private http: HttpClient) {
+    // this is just to test to see if the repository is working, no HTTP web service yet. pg.60
+    //this.product = JSON.parse(document.getElementById("data").textContent);
+
+    // using Http web service instead of static data pg. 79
+    this.getProduct(1);
+  }
+  
+  getProduct(id: number) {
+    // This is an observable method and returns an Observable<Product> object, kind of like an asynchronous .NET task. pg. 80
+    this.http.get<Product>("/api/products/" + id)
+      .subscribe(p => this.product = p);
+  }
 }
